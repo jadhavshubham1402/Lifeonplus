1b97cf6b2d389d0c8eb2a235ae9b2506b8dd8b28
Shubham Jadhav <ceo@lifeonplus.com>
2026-06-03 21:24:07 +0530

    changes are done

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -187,10 +187,6 @@ const Footer = () => {
         </div>
       </div>
       <div className="container py-16">
-        <h2 className="text-center text-white text-4xl font-bold mb-12">
-          LifeOnPlus Technologies India Private Limited
-        </h2>
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           {/* Registered Office */}
           <div>
